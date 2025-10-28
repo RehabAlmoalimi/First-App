89d7ca8d0895759b46ddae8bc1c37b1b89b509fe
RehabAlmoalimi <rehabmajeed2024@gmail.com>
2025-10-28 22:16:40 +0300

    correct the link

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pges.app.PagesConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
